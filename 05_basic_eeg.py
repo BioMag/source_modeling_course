0964727d4951cc563ad438a78b299a3d2b1c928c
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2022-10-22 22:46:25 +0300

    added filtering

---
+++ 05_basic_eeg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+MNE - basic operations on EEG file - filtering
+"""
+
+#%% Cell #0
+""" Load the data
+"""
+import mne
+INP_FNAME = '~/scratch/ERP-CORE_Subject-001_Task-Flankers_eeg_mark_bad.fif'
+raw_data_obj = mne.io.Raw(INP_FNAME, preload=True)
+
+
+#%% Cell #1
+""" Do the high-pass filtering to remove the drift
+"""
+CUTOFF_FREQ = 0 # Hz, what frequency we should use?
+raw_filtered = raw_data_obj.copy()      # The filtering is done in-place
+raw_filtered.filter(l_freq=CUTOFF_FREQ, h_freq=None)
+
+raw_filtered.plot()
+
+""" Check that the filtering fixed the drift
+"""
+
+
+#%% Cell #2
+""" Save the filtered data
+"""
+OUT_FNAME = '~/scratch/ERP-CORE_Subject-001_Task-Flankers_eeg_mark_bad_filt.fif'
+raw_filtered.save(OUT_FNAME)
