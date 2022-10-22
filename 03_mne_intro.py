87b37e5541bc80613342bf41af3de342c99d489b
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2022-10-22 12:19:19 +0300

    wip

--- 03_mne_intro.py
+++ 03_mne_intro.py
@@ -4,36 +4,41 @@
 The basics of MNE
 """
 
-#%% Cell 0
+#%% Cell #0
 """ MNE is Python package
 """
 import mne              # Let's load MNE
 print(mne.__version__)  # And check it's version
 
 
-#%% Cell 1
+#%% Cell #1
 """ Let's load some EEG data
 """
 FNAME = '/home/andrey/storage/Data/mne_data/MNE-ERP-CORE-data/ERP-CORE_Subject-001_Task-Flankers_eeg.fif'
 raw_data_obj = mne.io.Raw(FNAME, preload=True)
 
 
-#%% Cell 2
+#%% Cell #2
 """ Let's also look at the data
 """
 raw_data_obj.plot()
 
 
-#%% Cell 3
+#%% Cell #3
 """ Now, let's look at the file's info attribute
 """
 print(raw_data_obj.info)
-""" Let's play around with the info attribute. Can you figure out the subject's
-    sex?
+""" Explore raw_data_obj and raw_data_obj.info using spyder:
+    1. Use TAB after . (e.g. type raw_data_obj. in the console and press TAB)
+       to get available methods/attributes
+    2. Explore variables in the variable explorer
+    3. Use online help -- try using Ctrl-I on various things
+
+    Can you figure out the subject's sex?
 """
 
 
-#%% Cell 4
+#%% Cell #4
 """ What about the actual numerical measurement data? Here's the
     straightforward way
 """
@@ -46,7 +51,7 @@ print('We have %i channels of %i samples each' % (n_chan, n_samp))
 # t_tot = 
 
 
-#%% Cell 5
+#%% Cell #5
 """ Let's get the data for FC3 ...
 """
 ch_names = raw_data_obj.info.ch_names
@@ -54,14 +59,14 @@ FC3_idx = ch_names.index('FC3')
 FC3_data = raw_data[FC3_idx,:]
 
 
-#%% Cell 6
+#%% Cell #6
 """ ... and the corresponding time index ...
 """
 import numpy as np
 t = np.linspace(0, t_tot, n_samp)
 
 
-#%% Cell 7
+#%% Cell #7
 """ ... and plot the data
 """
 import matplotlib.pylab as plt
@@ -72,7 +77,7 @@ plt.show()
 """
 
 
-#%% Cell 8
+#%% Cell #8
 """ A cool pythonic way to do the same thing: indexing the raw_data_obj
     directly
 """
--- setup.md
+++ setup.md
@@ -1,3 +1,5 @@
+Tested on Windows 10 and Ubuntu 22.04 LTS.
+
 # Install miniconda
 
 # Create MNE environment
@@ -6,12 +8,23 @@ The environment contains mne + some useful packages (numpy, matplotlib, etc). Ch
 
 # Install Spyder
 Install the spyder package + some optional dependencies recommended by the author.  
+`conda create -n spyder spyder numpy scipy pandas matplotlib sympy cython`  
+  
+On Ubuntu, add PyQtWebEngine to the list of packages to be installed  
 `conda create -n spyder spyder numpy scipy pandas matplotlib sympy cython PyQtWebEngine`
 
-# Setup Spyder to use python interpreter from the mne environment
+# Setup Spyder
+Open spyder. From the miniconda prompt run  
+`conda activate spyder`  
+`spyder`  
+  
+Setup Spyder to use python interpreter from the mne environment. In Spyder: Tools -> Preferences -> Python interpreter -> Use the following Python intepreter -> select the interpreter for the mne environment.  
+  
+Setup Spyder to plot graphs in separate windows: In Spyder: Tools -> Preferences -> IPython console -> Graphics -> Backend -> Qt5.  
+Restart Spyder.  
 
 # Install FreeSurfer
-Download freesurfer-linux-ubuntu22_amd64-7.3.2.tar.gz to ~/opt  
+Doesn't work on Windows. Download freesurfer-linux-ubuntu22_amd64-7.3.2.tar.gz to ~/opt  
 `cd ~/opt`  
 `tar -zxpf freesurfer-linux-ubuntu22_amd64-7.3.2.tar.gz`  
 
